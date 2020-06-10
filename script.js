c320a3f7c73f2859158b425b39fa868cc326606d
AnOrdinaryUsername <kylemasa2@gmail.com>
2020-06-09 22:30:23 -0700

    Utilize class for recording char count on display

--- script.js
+++ script.js
@@ -1,65 +1,99 @@
-const clearDisplay = () => document.querySelector('.results').textContent = "";
+class Operations {
+    constructor(count) {
+        this.count = count;
+    }
 
-const deleteLastChar = () => {
-    let number = document.querySelector('.results').textContent;
-    document.querySelector('.results').textContent = number.slice(0, -1);
+    incrementCount() {
+        return this.count += 1;
+    }
+
+    decrementCount() {
+        return this.count -= 1;
+    }
+
+    resetCount() {
+        this.count = 0;
+    }
+
+    maxChars() {
+        return (this.count === 9);
+    }
+
+}
+
+const clearDisplay = (object) =>  {
+    document.querySelector('.results').textContent = "";
+    object.resetCount();
 };
 
-const topButtons = () => {
+const deleteLastChar = (object) => {
+    let number = document.querySelector('.results').textContent;
+    document.querySelector('.results').textContent = number.slice(0, -1);
+    object.decrementCount();
+};
+
+const deletion = (object) => {
     document.querySelector('#clear').addEventListener('click', function (e) {
-        clearDisplay();
+        clearDisplay(object);
     });
 
     document.querySelector('#delete').addEventListener('click', function (e) {
-        deleteLastChar();
+        deleteLastChar(object);
     });
 };
 
-const updateDisplay = (character) => {
+const denkoIsPresent = () => { 
+    return (document.querySelector('.results').textContent === '(´・ω・`)'); 
+};
+
+const adjustText = () => { 
+    let display = document.querySelector('.results');
+    display.style.textAlign = 'end';
+    display.style.fontSize = '6rem';
+    display.style.padding = '1rem'; 
+};
+
+const updateDisplay = (object, character) => {
+    if (object.maxChars()) return;
+
     let existingChar = document.querySelector('.results').textContent;
     document.querySelector('.results').textContent = existingChar.concat("", character);
-}
+    object.incrementCount();
+};
 
-const pressButtonEvent = () => {
+const pressButtonEvent = (object) => {
     document.querySelector('.buttons-wrapper').addEventListener('click',
         function (e) {
+            if (e.target.tagName == "DIV") return;
+            if (denkoIsPresent()) clearDisplay(object);
+
+            adjustText();
             let button = e.target.id;
-            switch (button) {
-                case "1":
-                    updateDisplay(1);
-                    break;
-                case "2":
-                    updateDisplay(2);
-                    break;
-                case "3":
-                    updateDisplay(3);
-                    break;
-                case "4":
-                    updateDisplay(4);
-                    break;
-                case "5":
-                    updateDisplay(5);
-                    break;
-                case "6":
-                    updateDisplay(6);
-                    break;
-                case "7":
-                    updateDisplay(7);
-                    break;
-                case "8":
-                    updateDisplay(8);
-                    break;
-                case "9":
-                    updateDisplay(9);
-                    break;
-                case "0":
-                    updateDisplay(0);
-                    break;
-                default:
-                    break;
+    
+            if (!isNaN(button)) {
+                const numberButton = Array.from({length: 10}, (v, i) => i);
+
+                let index = parseInt(button);
+                updateDisplay(object, numberButton[index]);
+            } 
+            else if (isNaN(button)) {
+                /* TODO
+                const symbolButton = ['decimal', 'parentheses', 'squared', 
+                        'reciprocal', 'exponent', 'sq-rt', 'division', 
+                        'multiplication', 'subtraction', 'addition', 'compute'];
+                let id = (element) => element === button;
+
+                let index = symbolButton.findIndex(id); */
+                
             }
         });
+};
+
+function main() {
+    let displayScreen = new Operations(0);
+
+    deletion(displayScreen);
+    pressButtonEvent(displayScreen);
 }
 
-topButtons();
-pressButtonEvent();
+main();
