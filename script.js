7cf7bb970f980f0afbbecef7da1487fe90d35f74
AnOrdinaryUsername <kylemasa2@gmail.com>
2020-06-11 23:07:00 -0700

    Add functionality for math symbol buttons

--- script.js
+++ script.js
@@ -1,99 +1,125 @@
-class Operations {
-    constructor(count) {
-        this.count = count;
-    }
+'use strict';
+/**
+ * Records the amount of chars on calculator display.
+ */
+class CharCounter {
+  /** @param {number} count The counter to store chars. */
+  constructor(count) {
+    /** @private {number} */
+    this.count = count;
+  }
 
-    incrementCount() {
-        return this.count += 1;
-    }
+  /** @param {string} str The string added to display.
+    * @return {number} */
+  incrementCount(str) {
+    return this.count += str.length;
+  }
 
-    decrementCount() {
-        return this.count -= 1;
-    }
+  /** @return {number} */
+  decrementCount() {
+    return this.count -= 1;
+  }
 
-    resetCount() {
-        this.count = 0;
-    }
+  /** @return {void} */
+  resetCount() {
+    this.count = 0;
+  }
 
-    maxChars() {
-        return (this.count === 9);
-    }
-
+  /** @return {boolean} */
+  maxChars() {
+    return (this.count >= 12);
+  }
 }
 
-const clearDisplay = (object) =>  {
-    document.querySelector('.results').textContent = "";
-    object.resetCount();
+const clearDisplay = (object) => {
+  document.querySelector('.results').textContent = '';
+  object.resetCount();
 };
 
 const deleteLastChar = (object) => {
-    let number = document.querySelector('.results').textContent;
-    document.querySelector('.results').textContent = number.slice(0, -1);
-    object.decrementCount();
+  const number = document.querySelector('.results').textContent;
+  document.querySelector('.results').textContent = number.slice(0, -1);
+  object.decrementCount();
 };
 
 const deletion = (object) => {
-    document.querySelector('#clear').addEventListener('click', function (e) {
-        clearDisplay(object);
-    });
+  document.querySelector('#clear').addEventListener('click', function(e) {
+    clearDisplay(object);
+  });
 
-    document.querySelector('#delete').addEventListener('click', function (e) {
-        deleteLastChar(object);
-    });
+  document.querySelector('#delete').addEventListener('click', function(e) {
+    deleteLastChar(object);
+  });
 };
 
-const denkoIsPresent = () => { 
-    return (document.querySelector('.results').textContent === '(´・ω・`)'); 
+const denkoIsPresent = () => {
+  return (document.querySelector('.results').textContent === '(´・ω・`)');
 };
 
-const adjustText = () => { 
-    let display = document.querySelector('.results');
-    display.style.textAlign = 'end';
-    display.style.fontSize = '6rem';
-    display.style.padding = '1rem'; 
+const adjustText = () => {
+  const display = document.querySelector('.results');
+  display.style.textAlign = 'end';
+  display.style.fontSize = '6rem';
+  display.style.padding = '1rem';
 };
 
-const updateDisplay = (object, character) => {
-    if (object.maxChars()) return;
+const updateDisplay = (object, string) => {
+  if (object.maxChars()) return;
 
-    let existingChar = document.querySelector('.results').textContent;
-    document.querySelector('.results').textContent = existingChar.concat("", character);
-    object.incrementCount();
+  const existingChar = document.querySelector('.results').textContent;
+  document.querySelector('.results').textContent =
+          existingChar.concat('', string);
+  object.incrementCount(string);
+};
+
+const checkParentheses = (object) => {
+  const oldString = Array.from(document.querySelector('.results').textContent);
+  const firstParen = (element) => element === '(';
+
+  if (oldString.findIndex(firstParen) === -1) updateDisplay(object, '(');
+  else updateDisplay(object, ')');
 };
 
 const pressButtonEvent = (object) => {
-    document.querySelector('.buttons-wrapper').addEventListener('click',
-        function (e) {
-            if (e.target.tagName == "DIV") return;
-            if (denkoIsPresent()) clearDisplay(object);
+  document.querySelector('.buttons-wrapper').addEventListener('click',
+      function(e) {
+        if (e.target.tagName == 'DIV') return;
+        if (denkoIsPresent()) clearDisplay(object);
 
-            adjustText();
-            let button = e.target.id;
-    
-            if (!isNaN(button)) {
-                const numberButton = Array.from({length: 10}, (v, i) => i);
+        adjustText();
+        const button = e.target.id;
 
-                let index = parseInt(button);
-                updateDisplay(object, numberButton[index]);
-            } 
-            else if (isNaN(button)) {
-                /* TODO
-                const symbolButton = ['decimal', 'parentheses', 'squared', 
-                        'reciprocal', 'exponent', 'sq-rt', 'division', 
-                        'multiplication', 'subtraction', 'addition', 'compute'];
-                let id = (element) => element === button;
+        if (!isNaN(button)) {
+          const numberButton = Array.from({length: 10}, (v, i) => i);
 
-                let index = symbolButton.findIndex(id); */
-                
-            }
-        });
+          const index = parseInt(button);
+          updateDisplay(object, `${numberButton[index]}`);
+        } else if (isNaN(button)) {
+          if (button === 'decimal') updateDisplay(object, '.');
+          // Quick note, parentheses has a different function from the rest
+          else if (button === 'parentheses') checkParentheses(object);
+          else if (button === 'squared') updateDisplay(object, '^2');
+          else if (button === 'reciprocal') updateDisplay(object, '^-1');
+          else if (button === 'exponent') updateDisplay(object, '^');
+          else if (button === 'addition') updateDisplay(object, '+');
+          else if (button === 'subtraction') updateDisplay(object, '-');
+          else if (button === 'sq-rt') {
+            updateDisplay(object, String.fromCharCode(0x221A));
+          } else if (button === 'division') {
+            updateDisplay(object, String.fromCharCode(0x00F7));
+          } else if (button === 'multiplication') {
+            updateDisplay(object, '*');
+          }
+        }
+      });
 };
 
-function main() {
-    let displayScreen = new Operations(0);
+const main = () => {
+  const INIT = 0;
+  const displayScreen = new CharCounter(INIT);
 
-    deletion(displayScreen);
-    pressButtonEvent(displayScreen);
-}
+  deletion(displayScreen);
+  pressButtonEvent(displayScreen);
+};
 
 main();
